b23f2a45b7cb3539c04f121b1a376ec201eaca7d
chao1405031 <3389822838@qq.com>
2018-04-30 21:20:00 +0800

    测试三条输出

---
+++ src/mygit/my2117.java
@@ -0,0 +1,16 @@
+package mygit;
+
+public class my2117 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("现在是9点十七分");
+		
+		System.out.println("将提交代码了哦");
+		
+		System.out.println("你大爷的  ，，还不成功？？");
+
+	}
+
+}
